b6efc8d943849befbbfa072f975a638150ae03c9
Mohan Lal <mhnuk2007@gmail.com>
2025-10-05 21:06:11 +0500

    Add project initial setup files

---
+++ src/test/java/com/learning/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.taskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
